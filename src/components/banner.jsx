1406d37c1ab2e8b6a7774cf65cb4a35214de5d42
esther odedoyin <estherodedoyin1@gmail.com>
2025-05-13 23:23:57 +0100

    worked on hero section

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -13,7 +13,7 @@ const Banner = () => {
   // bg-[#e4e5e0]
   return (
  
-<div className="xl:mt-20 mt-44 bg-[#e4ee0] sm:p-12 md:mt-24 p-2"
+<div className="xl:mt-20 mt-44 bg-[#e4ee0] sm:p-12 md:mt-24 "
 >
   <div className="md:h-[420px] h-[500px] w-full xl:flex lg:flex md:flex sm:flex flex sm:justify-center justify-center   bg-cover bg-center  md:bg-none bg-[url('cocoModel1.png')] ">
     <div className="md:w-3/5 xl:h-full w-full flex justify-center   bg-[#e4e5e0]   sm:h-full h-32    md:mt-56 sm:mt-0 mt-56   sm:border-[#36053c] sm:border-[12px] sm:border-r-0">
@@ -21,7 +21,7 @@ const Banner = () => {
       <img src="https://res.cloudinary.com/dz6yrbkai/image/upload/v1747173805/cocoEssentialsImages/cocogif.gif" alt="" />
     </div>
 
-<div className=" md:w-2/5 xl:flex justify-center p-2 sm:p-0  hidden md:block border-t-[13px] border-b-[13px] border-r-[12px] border-[#36053c] ">
+<div className=" md:w-2/5 xl:flex justify-center  sm:p-0  hidden md:block border-t-[13px] border-b-[13px] border-r-[12px] border-[#36053c] ">
 <img
         src="cocoModel1.png"
         alt="CocoBanner"
